651a0757476a58d640e40487f8d1facf4e0f775e
Frank <frankkashere@gmail.com>
2025-12-15 18:39:08 +0300

    Add : Added frontend with tailwind css

---
+++ ds_client/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-700">
+      <h1 className="text-xl font-bold text-red">
+        Tailwind v4 is working 🚀
+      </h1>
+    </div>
+  );
+}
