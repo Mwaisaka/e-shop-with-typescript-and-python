45df7cb17f4843a4a2748dbb1e56c94b8aeec77f
Frank <frankkashere@gmail.com>
2025-12-28 11:34:46 +0300

    Add : Added backend APIs for editing  and deleting categories

--- ds_server/categories/urls.py
+++ ds_server/categories/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import category_list_create
+from .views import category_list_create, category_detail
 
 urlpatterns = [
-    path("categories-list-create/", category_list_create)
+    path("categories-list-create/", category_list_create),
+    path("category-detail/<int:category_id>/", category_detail)
 ]
--- ds_server/categories/views.py
+++ ds_server/categories/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import CategorySerializer
 
-
+#View category list or Create category item
 @api_view(["GET", "POST"])
 def category_list_create(request):
     if request.method == "GET":
@@ -14,6 +14,39 @@ def category_list_create(request):
         serializer = CategorySerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status = 200)
+            return Response(serializer.data, status = 201)
         return Response(serializer.errors, status = 400)
+
+#RETRIEVE + UPDATE + DELETE category items
+@api_view(["GET", "PUT", "PATCH", "DELETE"])
+def category_detail(request, category_id):
+    try:
+        category = Category.objects.get(id=category_id)
+    except Category.DoesNotExist:
+        return Response(
+            {"error": "Category not found."},
+            status=404
+        )
+    if request.method=="GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CategorySerializer(category, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    elif request.method == "PATCH":
+        serializer = CategorySerializer(category, data = request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(
+            {"message": "Category deleted successfully"},
+            status=200
+        )
         
+    
