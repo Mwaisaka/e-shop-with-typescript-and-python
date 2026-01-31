bf36a02f7e12ec8f3ba8ad4ccf2d05c9afdb2c58
Frank <frankkashere@gmail.com>
2026-01-31 11:46:39 +0300

    Update : Updated product views and serializer to allow filter products by rating and price

--- ds_server/products/views.py
+++ ds_server/products/views.py
@@ -2,39 +2,62 @@ from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.db.models import Q
+from django.db.models import Q, Avg
 
-#View product list or create a product
+# View product list or create a product
 @api_view(["GET","POST"])
 def product_list_create(request):
     if request.method == "GET":
         q = request.GET.get("q","")
         category = request.GET.get("category")
-        products = Product.objects.select_related("category").all()
+        min_price = request.GET.get("min_price")
+        max_price = request.GET.get("max_price")
+        rating = request.GET.get("rating")
+        
+        # products = Product.objects.select_related("category").all()
+        
+        products = Product.objects.select_related("category").annotate(
+            avg_rating=Avg("reviews__rating")
+    )
+        
+        # Search product by name or category
         if q:
-            products=products.filter(
+            products = products.filter(
                 Q(name__icontains=q) | Q(category__name__icontains=q)
             )
+        
+        # Filter category slug
         if category:
-            products=products.filter(category__slug=category)
+            products = products.filter(category__slug=category)
+        
+        # Filter by price
+        if min_price:
+            products = products.filter(price__gte=min_price)
+        
+        if max_price:
+            products = products.filter(price__lte=max_price)
+        
+        # Filter by rating
+        if rating:
+            products = products.filter(reviews__rating__gte=rating).distinct()
             
         serializer = ProductSerializer(
             products, 
             many=True,
-            context ={"request": request}
+            context={"request": request}
             )
         return Response(serializer.data, status=200)
     elif request.method == "POST":
         serializer = ProductSerializer(
-            data = request.data,
-            context ={"request": request}
+            data=request.data,
+            context={"request": request}
             )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
 
-#Retrieve, Update & Delete a Single Product
+# Retrieve, Update & Delete a Single Product
 @api_view(["GET", "PUT","PATCH","DELETE"])
 def product_detail(request, product_id):
     try:
@@ -47,7 +70,7 @@ def product_detail(request, product_id):
     if request.method == "GET":
         serializer = ProductSerializer(
             product,
-            context ={"request": request}
+            context={"request": request}
             )
         return Response(serializer.data)
     elif request.method == "PUT":
