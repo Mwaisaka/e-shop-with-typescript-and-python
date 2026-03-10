07ef0a148024263ddd7e4d1ce5997971a39d8e7e
Frank <frankkashere@gmail.com>
2026-03-10 12:45:39 +0300

    Update : Updated static files file

--- ds_server/ds_server/settings.py
+++ ds_server/ds_server/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
