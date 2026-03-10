0785a2072bb4b41b1c0bcfdaec19bf628adbf92c
Frank <frankkashere@gmail.com>
2026-03-10 21:41:55 +0300

    Update : Updated django server settings

--- ds_server/ds_server/settings.py
+++ ds_server/ds_server/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'ds_server.urls'
 
