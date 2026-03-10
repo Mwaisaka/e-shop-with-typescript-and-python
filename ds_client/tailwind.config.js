cfc40a39321f1ac49a2066dfdfb0bd48420cabf4
Frank <frankkashere@gmail.com>
2026-03-10 22:20:52 +0300

    Update : Updated tailwind settings

--- ds_client/tailwind.config.js
+++ ds_client/tailwind.config.js
@@ -5,6 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
      "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    "flex",
+    "grid",
+    "hidden",
+    "block",
+    "w-64",
+    "w-72",
+    "md:grid-cols-2",
+    "md:grid-cols-3",
+    "lg:grid-cols-4",
+  ],
   theme: {
     extend: {},
   },
