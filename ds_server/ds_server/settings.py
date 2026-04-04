d1a95e1252d876c268a76e42f91994e212bae8b8
Frank <frankkashere@gmail.com>
2026-04-04 12:51:36 +0300

    Update : Updated landing page

--- ds_server/ds_server/settings.py
+++ ds_server/ds_server/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7hmh+-i#l1&0304dz(cprxk%=alg=ywbc8x^sw$mh6=&zd@rw@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
